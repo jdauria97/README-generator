ae3d02e42bf4b34b53735c171b3adc5c5ef85849
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-02-07 10:42:59 -0500

    adds renderLicenseLink content

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,7 +18,21 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {};
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT License': 
+        return 'https://mit-license.org/';
+    
+    case 'GNU General Public License (GPL)':
+        return 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+    
+    case 'Apache License 2.0': 
+        return 'https://www.apache.org/licenses/LICENSE-2.0';
+    
+    default:
+        return '';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
