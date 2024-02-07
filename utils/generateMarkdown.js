759b26bfcf0e3684ddbf2f72f1e3a54914a32119
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2024-02-07 10:39:33 -0500

    adds renderLicenseBadge content

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT License': 
+        return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    
+    case 'GNU General Public License (GPL)':
+        return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    
+    case 'Apache License 2.0': 
+        return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    
+    default:
+        return '';
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
 `;
-}
+};
 
 module.exports = generateMarkdown;
